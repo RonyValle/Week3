8c4a28032970f0109eeeb8884e096ccdf54d500a
Rony Valle Lopez <35381394+RonyValle@users.noreply.github.com>
2018-05-07 10:08:48 -0500

    Update ch3.sh

--- ch3.sh
+++ ch3.sh
@@ -2,6 +2,12 @@
 #Rony Valle Lopez
 #Chapter3 Script
 
+#take input from the echo command and send it to dev/null
+echo this will be discarted by the kernel > /dev/null
+
+#monitor and exporer the sda device using udevadm
+udevadm info --query=all --name=/dev/sda
+
 #The following script list your connected sda devices and sends the information to a file call here which displays the information gathered by the script.
 echo
 echo "Your attached devices will be opened outside the terminal: "
